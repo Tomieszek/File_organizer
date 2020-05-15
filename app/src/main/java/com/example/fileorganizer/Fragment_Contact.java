df5a53d22bb0b6703d7a0f58a3b5792ac98bd334
Tomieszek <tomek.ozog98@gmail.com>
2020-05-15 15:23:18 +0200

    Added Files and Call permissions

--- app/src/main/java/com/example/fileorganizer/Fragment_Contact.java
+++ app/src/main/java/com/example/fileorganizer/Fragment_Contact.java
@@ -1,19 +1,48 @@
 package com.example.fileorganizer;
 
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Fragment_Contact extends Fragment {
-    //Todo: Implement Contact Fragment
+
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_contact, container, false);
+
+
+
+
+
+
+
+
+//       Button btnCall = (Button)findViewById(R.id.btnCall);
+//
+//        btnCall.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(Intent.ACTION_DIAL);
+//                intent.setData(Uri.parse("tel:0123456789"));
+//                startActivity(intent);
+//            }
+//        });
     }
+
+
+
 }
+
+
